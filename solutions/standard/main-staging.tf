14c7ed2713087e7fb69e3169602c09fa7e1342ca
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-01-15 13:55:07 -0500

    add do nothing terraform for ys1-staging testing only

---
+++ solutions/standard/main-staging.tf
@@ -0,0 +1,32 @@
+
+resource "time_sleep" "wait_11_seconds" {
+    create_duration = "11s"
+}
+
+output "resource_group_name" {
+    value       = "resource_group_name"
+}
+
+output "resource_group_id" {
+    value       = "resource_group_id"
+}
+
+output "secrets_manager_guid" {
+    value       = "secrets_manager_guid"
+}
+
+output "secrets_manager_id" {
+    value       = "secrets_manager_id"
+}
+
+output "secrets_manager_name" {
+    value       = "secrets_manager_name"
+}
+
+output "secrets_manager_crn" {
+    value       = "secrets_manager_crn"
+}
+
+output "secrets_manager_region" {
+    value       = "secrets_manager_region"
+}
